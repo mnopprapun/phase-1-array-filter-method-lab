dc8190fd866a2856d32e39251da8d46451b6c89d
Marc Nopprapun <mnopprapun@gmail.com>
2021-04-08 18:45:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+  return drivers.filter(match =>
+     match.toUpperCase() === name.toUpperCase()); 
+  }
+
+function fuzzyMatch(drivers, name){
+return drivers.filter(match => match.startsWith(name))
+}
+
+function matchName(drivers) {
+    return drivers.filter(match =>
+       match.name === "Bobby" )
+    }
